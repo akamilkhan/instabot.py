3022df2865a7de5071c0a1d974cc7c1fa6775551
Kamil Khan <kamil.khan@emumba.com>
2018-10-21 21:48:22 +0500

    Mode 2 and 5 crash fix

--- src/likers_protocol.py
+++ src/likers_protocol.py
@@ -7,6 +7,7 @@ from .post_page import get_user_id_post_page
 from .username_checker import username_checker
 
 
+
 def likers_protocol(self):
     if len(self.media_by_user) > 0:
         # You have media_id to like:
@@ -15,10 +16,19 @@ def likers_protocol(self):
             self.current_index, len(self.media_by_user))
         self.write_log(log_string)
 
-        if self.media_by_user[self.
-                              current_index]["likes"]["count"] >= 10 and self.media_by_user[self.
-                                                                                            current_index]["likes"]["count"] < 100:
-            get_user_id_post_page(
-                self, self.media_by_user[self.current_index]["code"])
+        likes_count = get_likes_count(self)
+
+        if likes_count >= 10 and likes_count < 100:
+            get_user_id_post_page(self, self.media_by_user[self.current_index]["code"])
             username_checker(self)
         del self.media_by_user[self.current_index]
+
+
+def get_likes_count(self):
+    if self.current_index in self.media_by_user:
+        if "node" in self.media_by_user[self.current_index]:
+            if "edge_liked_by" in self.media_by_user[self.current_index]["node"]:
+                if "count" in self.media_by_user[self.current_index]["node"]["edge_liked_by"]:
+                    return self.media_by_user[self.current_index]["node"]["edge_liked_by"]["count"]
+
+    return -1;
